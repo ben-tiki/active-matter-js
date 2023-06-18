7cdcdc36f62d7c3fa1802a400a3329b092cd4baf
ben-tiki <51ir42824557@campus.economicas.uba.ar>
2023-06-18 01:21:51 -0300

    code-refactor

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Active Matter Simulation
-This projects creates Boids or "Bird-like" objects moving on the canvas, simulatating the flocking behaviour of birds.
+This project demonstrates a simulation of 'Boids' - bird-like objects - interacting on a canvas. Boids follow simple, yet interesting rules which create emergent flocking behaviour.
 
-### Rules: 
-
- 1. Birds start at a random position on the canvas.
- 2. Each bird has a random initial x_vector and y_vector (defining the direction it will move)
- 3. If a bird is within the interaction radius of another it is considered a "neighbor"
- 4. If neighbor, the bird will allign its movement with other birds
- 5. Each bird has a random "noise" in their movement
+### Simulation Mechanics
+ 1. **Initial Setup**: Each Boid starts in a random position on the canvas, moving in a random direction.
+ 2. **Neighbour Detection**: If another Boid is within a certain interaction radius, it is considered a neighbour.
+ 3. **Alignment**: When a Boid identifies a neighbour, it adjusts its direction to align with the other Boids within its interaction radius.
+ 4. **Movement Noise**: Each Boid exhibits a random degree of 'noise' in its movement, preventing purely straight-line motion and adding some variability.
+ 5. **Wraparound**: Boids that reach the edge of the canvas will appear on the opposite edge, maintaining constant population within the visible space.
  
+### Running the Simulation
 https://user-images.githubusercontent.com/101474762/187568340-0e1ef3d4-2b64-4599-b40a-96d29c8ca485.mov
 
 ### Parameters: 
-1. velocity (speed in which the birds traverse the canvas)
-2. noise (random movement of each bird. If 0 the birds will move on a straight line)
-3. box_size (universe in which the birds will move)
-4. interaction_radius (distance in which another bird is considered a neighbor)
-5. number_birds (amount of birds to populate the simulation)
-6. bird_size (size of the bird on the canvas. Doesent affect the simulation)
+1. **Velocity**: Control the speed at which the Boids move across the canvas.
+2. **Noise**: Adjust the degree of random movement of each Boid. Setting this to 0 will result in Boids moving in straight lines.
+3. **Box Size**: Determine the size of the universe or the boundary in which the Boids move.
+4. **Interaction Radius**: Set the distance at which another Boid is considered a neighbour, influencing alignment behaviour.
+5. **Number of Boids**: Decide the total amount of Boids to populate the simulation.
+6. **Boid Size**: Change the size of each Boid on the canvas. Note that this parameter is purely visual and does not affect the simulation mechanics.
---
+++ index.js
@@ -0,0 +1,135 @@
+// CANVAS
+// ----------------------------------------
+const canvasElement = document.getElementById("animation-canvas");
+const canvasCtx = canvasElement.getContext("2d");
+
+// Makes the canvas fullscreen
+canvasElement.width = window.innerWidth;
+canvasElement.height = window.innerHeight;
+
+// Add event listener for canvas resizing
+window.addEventListener("resize", function () {
+    canvasElement.width = window.innerWidth;
+    canvasElement.height = window.innerHeight;
+    boundingBoxWidth = canvasElement.width;
+    boundingBoxHeight = canvasElement.height;
+});
+
+// INITIALIZE 
+// ----------------------------------------
+const boidVelocity = 2.5;
+const boidMovementNoise = 0.5;
+let boundingBoxWidth = canvasElement.width;
+let boundingBoxHeight = canvasElement.height;
+const boidInteractionRadius = boundingBoxWidth / 50;
+const boidNumber = 800;
+const boidSize = 4;
+const boidColor = "#C40000";
+
+// Initialize the boids
+const boids = Array.from({ length: boidNumber }, () => {
+    const directionAngle = Math.random() * Math.PI * 2;
+
+    return {
+        position: {
+            x: Math.random() * boundingBoxWidth,
+            y: Math.random() * boundingBoxHeight
+        },
+        velocity: {
+            x: Math.cos(directionAngle) * boidVelocity,
+            y: Math.sin(directionAngle) * boidVelocity
+        }
+    };
+});
+
+// MAIN LOOP
+// ----------------------------------------
+function main() {
+
+    // Clear the canvas from last frame
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    boids.forEach((boid, i) => {
+
+        // Apply forces
+        boid.position.x += boid.velocity.x;
+        boid.position.y += boid.velocity.y;
+
+        // Wrap around the bounding box
+        if (boid.position.x >= boundingBoxWidth) {
+            boid.position.x = 0;
+        } else if (boid.position.x <= 0) {
+            boid.position.x = boundingBoxWidth;
+        }
+
+        if (boid.position.y >= boundingBoxHeight) {
+            boid.position.y = 0;
+        } else if (boid.position.y < 0) {
+            boid.position.y = boundingBoxHeight;
+        }
+
+        // Find the mean direction of the other boids
+        let neighbourCount = 0;
+        let meanDirectionAngle = 0;
+        boids.forEach((otherBoid, j) => {
+            if (i !== j) {
+                const distance = Math.sqrt(
+                    (boid.position.x - otherBoid.position.x) ** 2 +
+                    (boid.position.y - otherBoid.position.y) ** 2
+                );
+
+                if (distance < boidInteractionRadius) {
+                    meanDirectionAngle += Math.atan2(
+                        otherBoid.velocity.y,
+                        otherBoid.velocity.x
+                    );
+                    neighbourCount += 1;
+                }
+            }
+        });
+
+        if (neighbourCount > 0) {
+            meanDirectionAngle /= neighbourCount;
+        }
+
+        // Add random movement
+        meanDirectionAngle += boidMovementNoise * (Math.random() - 0.5);
+
+        // Update the velocity vector
+        boid.velocity.x = Math.cos(meanDirectionAngle) * boidVelocity;
+        boid.velocity.y = Math.sin(meanDirectionAngle) * boidVelocity;
+
+        // Compute direction of the boid
+        const directionAngle = Math.atan2(boid.velocity.y, boid.velocity.x);
+
+        // Draw the boids as triangles
+        const trianglePoints = [
+            {
+                x: boid.position.x + boidSize * 2 * Math.cos(directionAngle),
+                y: boid.position.y + boidSize * 2 *  Math.sin(directionAngle)
+            },
+            {
+                x: boid.position.x + boidSize * Math.cos(directionAngle + 2 / 3 * Math.PI),
+                y: boid.position.y + boidSize * Math.sin(directionAngle + 2 / 3 * Math.PI)
+            },
+            {
+                x: boid.position.x + boidSize * Math.cos(directionAngle - 2 / 3 * Math.PI),
+                y: boid.position.y + boidSize * Math.sin(directionAngle - 2 / 3 * Math.PI)
+            }
+        ];
+
+        canvasCtx.beginPath();
+        canvasCtx.moveTo(trianglePoints[0].x, trianglePoints[0].y);
+        canvasCtx.lineTo(trianglePoints[1].x, trianglePoints[1].y);
+        canvasCtx.lineTo(trianglePoints[2].x, trianglePoints[2].y);
+        canvasCtx.closePath();
+        canvasCtx.fillStyle = boidColor;
+        canvasCtx.fill();
+    });
+
+    window.requestAnimationFrame(main);
+};
+
+// ANIMATION LOOP
+// ----------------------------------------
+window.requestAnimationFrame(main);
